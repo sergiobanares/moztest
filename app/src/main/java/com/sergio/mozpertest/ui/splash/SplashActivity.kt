d7ad9f3aa94e91ce6dda0c9ca38e47977e30be9f
Sergio Bañares <sergio.banares@gmail.com>
2022-09-11 20:29:46 -0300

    Change: SplashActivity Created

---
+++ app/src/main/java/com/sergio/mozpertest/ui/splash/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.sergio.mozpertest.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.sergio.mozpertest.ui.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
